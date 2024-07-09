430950d0e6f1feab05acac6ecc5890a80c2afd16
REIGaruki <gyakujo@mail.ru>
2024-07-09 12:53:32 +0300

    Methods

--- src/StringListImpl.java
+++ src/StringListImpl.java
@@ -1,71 +1,146 @@
+import java.util.Arrays;
+
 public class StringListImpl implements StringList{
+    private String[] stringList;
+    public StringListImpl() {
+        this.stringList = new String[0];
+    }
+
     @Override
     public String add(String item) {
-        return null;
+        int newSize = stringList.length + 1;
+        String[] strings = new String[newSize];
+        for (int i=0; i< stringList.length; i++) {
+            strings[i] = stringList[i];
+        }
+        strings[newSize - 1] = item;
+        stringList = strings;
+        return item;
     }
 
     @Override
     public String add(int index, String item) {
-        return null;
+        int newSize = stringList.length + 1;
+        String[] strings = new String[newSize];
+        for (int i=0; i< index; i++) {
+            strings[i] = stringList[i];
+        }
+        strings[index] = item;
+        for (int i=index+1; i< newSize; i++) {
+            strings[i] = stringList[i - 1];
+        }
+        stringList = strings;
+        return item;
     }
 
     @Override
     public String set(int index, String item) {
-        return null;
+        stringList[index] = item;
+        return item;
     }
 
     @Override
     public String remove(String item) {
-        return null;
+        int newSize = stringList.length - 1;
+        String[] strings = new String[newSize];
+        int index = this.indexOf(item);
+        for (int i=0; i< index; i++) {
+            strings[i] = stringList[i];
+        }
+        for (int i=index + 1; i< stringList.length; i++) {
+            strings[i - 1] = stringList[i];
+        }
+        stringList = strings;
+        return item;
     }
 
     @Override
     public String remove(int index) {
-        return null;
+        String item = stringList[index];
+        int newSize = stringList.length - 1;
+        String[] strings = new String[newSize];
+        for (int i=0; i< index; i++) {
+            strings[i] = stringList[i];
+        }
+        for (int i=index + 1; i< stringList.length; i++) {
+            strings[i - 1] = stringList[i];
+        }
+        stringList = strings;
+        return item;
     }
 
     @Override
     public boolean contains(String item) {
+        for (int i=0; i< stringList.length; i++) {
+            if (stringList[i].equals(item)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int indexOf(String item) {
-        return 0;
+        for (int i=0; i< stringList.length; i++) {
+            if (stringList[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(String item) {
-        return 0;
+        for (int i=stringList.length - 1; i>=0 ; i--) {
+            if (stringList[i].equals(item)) {
+                return stringList.length - 1 - i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public String get(int index) {
-        return null;
+        return stringList[index];
     }
 
     @Override
     public boolean equals(StringList otherList) {
-        return false;
+        StringListImpl that = (StringListImpl) otherList;
+        return Arrays.equals(stringList, that.stringList);
     }
 
     @Override
     public int size() {
-        return 0;
+        return stringList.length;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return (stringList.length == 0);
     }
 
     @Override
     public void clear() {
-
+        stringList = new String[0];
     }
 
     @Override
     public String[] toArray() {
-        return new String[0];
+        String[] strings = new String[stringList.length];
+        for (int i=0; i< stringList.length; i++) {
+            strings[i] = stringList[i];
+        }
+        return strings;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(stringList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(stringList);
     }
 }
